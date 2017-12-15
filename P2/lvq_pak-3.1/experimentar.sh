a8117cc6ebd5cc7e81f8edf99d3ea12f3eb18c2d
Luis <100330021@alumnos.uc3m.es>
2017-12-15 21:16:31 +0100

    Terminada experimentación lvq

--- P2/lvq_pak-3.1/experimentar.sh
+++ P2/lvq_pak-3.1/experimentar.sh
@@ -1,9 +1,9 @@
 echo "Experimentación lvq"
-mkdir 10
-./eveninit -din data/Train1.csv -cout 10/Train1.cod -noc 10
-./mindist -cin 10/Train1.cod
-./balance -din data/Train1.csv -cin 10/Train1.cod -cout 10/Trained1.cod
-./olvq1 -din data/Train1.csv -cin 10/Trained1.cod -cout 10/Trained1_2.cod -rlen 5000
-./accuracy -din data/Test1.csv -cin 10/Trained1_2.cod
-./classify -din data/Test1.csv -cin 10/Trained1_2.cod -dout 10/testOut1.txt
-./sammon -cin 10/Trained1_2.cod -cout 10/Trained.sam -ps -rlen 5000
+mkdir 180
+./eveninit -din data/Train1.csv -cout 180/Train1.cod -noc 180
+./mindist -cin 180/Train1.cod
+./balance -din data/Train1.csv -cin 180/Train1.cod -cout 180/Trained1.cod
+./olvq1 -din data/Train1.csv -cin 180/Trained1.cod -cout 180/Trained1_2.cod -rlen 5000
+./accuracy -din data/Test1.csv -cin 180/Trained1_2.cod
+./classify -din data/Test1.csv -cin 180/Trained1_2.cod -dout 180/testOut1.txt
+./sammon -cin 180/Trained1_2.cod -cout 180/Trained.sam -ps -rlen 5000
